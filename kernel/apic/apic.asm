28b625f937a0be3555b943e35b963f5421026b9d
Alexander Leonhardt <equinox.malexander@outlook.de>
2015-05-10 21:23:01 +0200

    Basic Code for virtual memory

--- kernel/apic/apic.asm
+++ kernel/apic/apic.asm
@@ -21,6 +21,28 @@ struc idt_entry
 	.reserved resd 1
 endstruc
 
+%define LOCAL_APIC_ID 0x20
+%define LOCAL_APIC_VERSION 0x30
+%define LOCAL_APIC_TPR 0x80
+%define LOCAL_APIC_APR 0x90
+%define LOCAL_APIC_PPR 0xA0
+%define LOCAL_APIC_EOI 0xB0
+%define LOCAL_APIC_LOGICALDST 0xD0
+%define LOCAL_APIC_DSTFORMAT 0xE0
+%define LOCAL_APIC_SPURIOUSVEC 0xF0
+%define LOCAL_APIC_ERRORSTATUS 0x230
+%define LOCAL_APIC_ICRLOW 0x300
+%define LOCAL_APIC_ICRHIGH 0x310
+%define LOCAL_APIC_LVTTIMER 0x320
+%define LOCAL_APIC_LVTTHERMALSENSOR 0x330
+%define LOCAL_APIC_PERFORMANCEMONIT 0x340
+%define LOCAL_APIC_LINT0 0x350
+%define LOCAL_APIC_LINT1 0x360
+%define LOCAL_APIC_LVTERROR 0x370
+%define LOCAL_APIC_INITIALCOUNT 0x380
+%define LOCAL_APIC_CURRENTCOUNT 0x390
+%define LOCAL_APIC_DIVIDECONFIG 0x3E0
+
 
 global setIDTGate
 ;rdi = address; rcx = int number
@@ -97,8 +119,73 @@ picMapInterrupts:
 	ret
 
 global startAllAPs
+; eax = start vector
 startAllAPs:
+	push rbx
+	push rcx
+	push rdx
+	push rax
+	mov eax, 1
+	cpuid
+
+	test edx, (1<<9)
+	jz .noAPIC
+
+	shr eax, 8
+	and al, 0xF
+
+	cmp al, 5
+	js .noAPIC
+
+	mov ecx, 0x1B
+	rdmsr
+	and ax, 0xF000
+	mov dword[ APIC_BASE ], eax
+
+	mov ebx, 0xC4500
+	mov dword[ eax + LOCAL_APIC_ICRLOW ], ebx
+	mov ebx, 0xFF000000
+	mov dword[ eax + LOCAL_APIC_ICRHIGH ], ebx
+
+	push rax
+	mov ebx, 100
+	.check:
+		in al, 0x92
+		sub ebx, 1
+		jnz .check
+
+	pop rax
+	mov ebx, 0xC4600
+	mov dword[ eax + LOCAL_APIC_ICRLOW ], ebx
+	mov ecx, 0xFF000000
+	mov dword[ eax + LOCAL_APIC_ICRHIGH ], ecx
+
+	push rax
+
+	in al, 0x92
+	in al, 0x92
+	in al, 0x92
+	in al, 0x92
 	
+	pop rax
+	pop rbx
+	or ebx, 0xC4600
+
+	mov dword[ eax + LOCAL_APIC_ICRLOW ], ebx
+	mov dword[ eax + LOCAL_APIC_ICRHIGH ], ecx
+
+
+	push qword 0
+
+	.noAPIC:
+		pop rax
+		pop rdx
+		pop rcx
+		pop rbx
+		ret
+	
+
+APIC_BASE dd 0
 
 IDT_LIMIT dw (256*16)-1
 IDT_Base dq 0
---
+++ kernel/memory/vmemory.asm
@@ -0,0 +1,10 @@
+%include "memory.inc"
+
+
+global mapPhysToVirt
+mapPhysToVirt:
+
+
+global InitialiseVirtMem
+;rax = PML4 Base addr
+InitialiseVirtMem:
